568b272d15b1fe1064a4f51af6753ff8effdbac4
Steve Guo <steve.guo@carleton.ca>
2023-08-08 23:23:29 -0400

    Update for GitHub in 2023

--- getLatestBuild.sh
+++ getLatestBuild.sh
@@ -2,30 +2,48 @@
 
 #
 # Quick-and-dirty script to retrieve the latest build of KoL Mafia, then launch it.
-# Obtained from builds.kolmafia.us
+# Obtained from GitHub
 # 
-# Play KoL at: http://www.kingdomofloathing.com
+# Play KoL at: https://www.kingdomofloathing.com
 #
 # - Zombie Feynman (#1886944), a Curious Character
+# - @scguo (on GitHub), a Bad Coder
 #
 
+if [ -f .env ]
+then
+    export $(grep -v '^#' .env | xargs)
+fi
+
 # Download the latest build ...
-LATEST=$(curl builds.kolmafia.us | grep -o 'http://builds.kolmafia.us/KoLmafia-[0-9][0-9][0-9][0-9][0-9].jar' | head -1)
+ LATEST=$(curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GH_PERSONAL_KEY" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/kolmafia/kolmafia/releases/latest \
+  | grep -o 'https://github.com/kolmafia/kolmafia/releases/download/r[0-9][0-9][0-9][0-9][0-9]/KoLmafia-[0-9][0-9][0-9][0-9][0-9].jar' | head -1)
+# LATEST='https://github.com/kolmafia/kolmafia/releases/download/r27528/KoLmafia-27528.jar'
+
+echo "Current latest jar is: $LATEST"
 
 # Check whether the current build is already present. If it is, don't bother downloading it again.
-# The filename for KoLmafia is 18 characters long, and that seems unlikely to change in the near future.
+# The filename for KoLmafia is still 18 characters long, and that seems unlikely to change in the near future.
 # This is a bit of a hacky solution, though ...
 
 if [ -f ${LATEST:(-18)} ]
 then
-    echo "Latest KoLmafia build already present." 
+    echo "Latest KoLmafia build already present: ${LATEST##*/}" 
 else
-    echo "Fetching latest KoLmafia build."
+    echo "Fetching latest KoLmafia build: ${LATEST##*/}"
+    echo "making a storage folder for old version/s..."
+    mkdir -p old_versions
+    echo "saving old versions..."
+    mv -v KoL*jar old_versions
     curl -O $LATEST
+    
 fi
 
 # Launch the newest build on-hand ...
-LAUNCH=$(ls *jar | head -1)
-java -jar $LAUNCH
+java -jar ${LATEST##*/}
 
 exit 0
